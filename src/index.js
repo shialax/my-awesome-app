45569bfcc52fe69cc3fa18b14195f84d0cace280
shialax <74329916+shialax@users.noreply.github.com>
2022-08-18 16:04:43 -0400

    changed units from metric to imperial

--- src/index.js
+++ src/index.js
@@ -74,7 +74,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "308f0849c624ffab4a06f38f76ca19a0";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
